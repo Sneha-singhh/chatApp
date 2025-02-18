3d1050ec6fd66b34c1902bf964800ae73cda6d25
Sneha-singhh <singhsneha98752@gmail.com>
2025-02-18 11:43:37 +0530

    now user can snd large file

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import { app, server } from "./lib/socket.js";
 // import path from "path";
 
 // const app = express();
-app.use(express.json());
+app.use(express.json({limit : '100mb'}));
 app.use(cookieParser());
 app.use(cors({
     origin:"http://localhost:5173",
