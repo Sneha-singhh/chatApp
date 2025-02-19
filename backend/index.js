f7f0134dd70f058d7c446327f5f91a1135a5ce1f
Sneha-singhh <singhsneha98752@gmail.com>
2025-02-19 19:55:55 +0530

    project completed

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 import { app, server } from "./lib/socket.js";
 
-// import path from "path";
+import path from "path";
 
 // const app = express();
 app.use(express.json({limit : '100mb'}));
@@ -23,7 +23,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5001
 const URI = process.env.MONGODB_URI
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 // connect to mongodb
 try {
@@ -42,13 +42,13 @@ try {
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
-//     app.get("*", (req, res) => {
-//       res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//     });
-//   }
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
