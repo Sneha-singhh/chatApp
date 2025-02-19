9d854ee5beeead52d3365aa99b8faaf084a66505
Sneha-singhh <singhsneha98752@gmail.com>
2025-02-19 21:21:07 +0530

    finally

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cors({
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const URI = process.env.MONGODB_URI
 
 const __dirname = path.resolve();
